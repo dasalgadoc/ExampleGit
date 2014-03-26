0c1a7dd9f52db130223058a9b6082fd1217dcab2
HogarSC <HogarSC@PC-MesaDiego>
2014-03-25 23:01:34 -0500

    HelloWorld and first commit

---
+++ ExampleGit/src/controller/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author Diego
+ */
+public class Main {
+    
+    public static void main (String args[]){
+        System.out.println("Hello World!");
+    }
+}
